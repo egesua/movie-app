0f5ddaed843d8ef715421e1de256bb0fee737477
egesua <egesu06@gmail.com>
2024-04-13 16:21:48 +0300

    routers, navbar created.

--- cinemania/src/App.jsx
+++ cinemania/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
-  return (
-    <>
-      Hello World.
-    </>
-  )
+  return <div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/auth" element={<h1>Auth</h1>} />
+        <Route path="/rated" element={<h1>Rated Page</h1>} />
+      </Routes>
+    </Router>
+  </div>;
 }
 
-export default App
+export default App;
